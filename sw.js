92b8feeded3b9ad8f1525e78a3d35bc65641e749
1Kamili <thesecondkamili@gmail.com>
2026-08-10 05:49:39 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'zenith-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    './index.html', './manifest.json', './icon-192.png', './icon-512.png'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
